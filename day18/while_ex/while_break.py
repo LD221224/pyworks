e48f4e392995fa4f55ff8ff6baf68e66d8c4ce17
LD221224 <dayeong2815@gmail.com>
2022-01-25 12:47:51 +0900

    day18

---
+++ day18/while_ex/while_break.py
@@ -0,0 +1,7 @@
+# while ~ break문
+i = 0   # 초깃값
+while True: # 콜론
+    i += 1  # 4칸 인덴트
+    if i > 10:
+        break
+    print("Hello~ ", i)
---
+++ day18/while_ex/while_hello.py
@@ -0,0 +1,5 @@
+# while문
+i = 0   # 초깃값
+while i < 10:   # 콜론
+    i += 1      # 4칸 인덴트
+    print("Hello~ ", i)
